aafdcf1da7e4e851480a2d4d288f16a4cbb8f9c7
CaioRangel <caio.rangel@lynxprocess.com>
2025-10-23 20:48:59 -0300

    Added printf about brutexx smell (is not true tho)

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 void main () {
 
 	printf("Ash is a cool guy!\n");
-	printf("And Brutexx is a stinky guy!");
+	printf("And Brutexx is a stinky guy!\n");
+	printf("And he becomes stinkier, the more u know him!\n");
+	printf("he should take a bath! because, stinky!");
 
   return;
 }
